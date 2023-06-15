d06ad366c5cbb50ab421e8eedbe84e8b7e90b2d7
ilhamz <ilham.wpt021@gmail.com>
2023-06-16 03:40:56 +0800

    ini react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>JOKOWI INI GENGS</h1>
+      <h1>AKAN HADIR</h1>
+      <h2>Tunggu Saja</h2>
+      <a href='https://github.com/ilhamsetiawanz'>INI GITHUB COY</a>
+
+      <footer>
+        <p>Projek Paling AMBISIUS ILHAM</p>
+
+      </footer>
     </div>
   );
 }
